ea09420bbd92bcab1933588750ac5c15d6bfff31
sandip-pathe <sandip.pathe@vit.edu.in>
2023-08-26 01:02:40 +0530

    new file

---
+++ README.md
@@ -0,0 +1,13 @@
+# Game : Sandy
+
+>>Statement: Its a multiplayer game, evryone gets a turn. When you get a turn you roll a die, you get a number from 1 to 6. If you get anything other than one then it get adds up in your score, which is intially set to zero. you can decide as much rolls as you want but if you get 1 the score becomes zero. the max score is 55 whoever hits 51 fist will be the # Winner
+
+# Algorithm
+1. generate a number btw 1-6
+2. ask player if they wanna roll again or stop
+    a. if stop : add and show the final score
+        if score >= 55
+            Winner
+        if not 
+            continue
+    b. if no : roll the dice again
---
+++ project1.py
@@ -0,0 +1,48 @@
+import random
+
+
+def roll():
+    min = 1
+    max = 6
+    roll = random.randint(min, max)
+    
+    return roll
+
+
+while True:
+    players = input("Enter Number of Players (2 - 4): ")
+    if players.isdigit():
+        players = int(players)
+        if 2 <= players <= 4:
+            break
+        else:
+            print("Please enter number between 2 and 4")
+    
+    else:
+        print("Invalid number")
+
+
+max_score = 50
+players_scores = [0 for _ in range(players)]
+current_score = 0
+
+while max(players_scores) < max_score:
+    
+    for player_idx in range(players):
+        print("\nPlayer number", player_idx + 1, "turn has just started!")
+        print("Your total score is:", players_scores[player_idx], "\n")
+
+
+
+        should_play = input("do you want to play (y)? ")
+        if should_play.lower() != "y":
+            break
+
+        value = roll()
+
+        if value == 1:
+            print("Its 1, try again")
+            current_score = 0
+            break
+        else:
+            current_score += value
