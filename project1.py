7454911a8b24291cc50905161dfa71e2ad51da85
sandip-pathe <sandip.pathe@vit.edu.in>
2023-08-26 15:22:13 +0530

    Project1 is completed

--- project1.py
+++ project1.py
@@ -1,6 +1,5 @@
 import random
 
-
 def roll():
     min = 1
     max = 6
@@ -22,31 +21,33 @@ while True:
         print("Invalid number")
 
 
-max_score = 50
 players_scores = [0 for _ in range(players)]
 
 
-while max(players_scores) < max_score:
+
+for player_idx in range(players):
     current_score = 0
-    for player_idx in range(players):
-        print("\nPlayer number", player_idx + 1, "turn has just started!")
-        print("Your total score is:", players_scores[player_idx], "\n")
-        
+    print("\nPlayer number", player_idx + 1, "turn has just started!")
+    print("Your total score is:", players_scores[player_idx], "\n")
+    
 
-        while True:
-            should_play = input("do you want to play (y)? ")
-            if should_play.lower() != "y":
-                break
+    while True:
+        should_play = input("do you want to play (y)? ")
+        if should_play.lower() != "y":
+            break
 
-            value = roll()
+        value = roll()
 
-            if value == 1:
-                print("Its 1, try again")
-                current_score = 0
-                break
-            else:
-                current_score += value
-                print("your Score is: ", current_score)
+        if value == 1:
+            print("Its 1, try again")
+            break
+        else:
+            current_score += value
+            print("your Score is: ", current_score)
 
-        players_scores[player_idx] += current_score
-        print(players_scores[player_idx])
+    players_scores[player_idx] += current_score
+
+highest_score = max(players_scores)
+winner = players_scores.index(highest_score)
+print("Player ", winner, " is a WINNER with Highest Score: ", highest_score)
+
