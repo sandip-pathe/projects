8290552b481037737d7aa0ff47da377c4d76e063
sandip-pathe <sandip.pathe@vit.edu.in>
2023-08-29 22:23:13 +0530

    new random file project

--- edu.py
+++ edu.py
@@ -1,1 +1,12 @@
-print("Welcome to the website")
+import random
+import sys
+
+def sandip(x):
+    number = random.randint(5,9)
+    number = number ** x
+    return number
+
+input_xx = sys.argv[1]
+if input_xx.isdigit():
+    input_xx = int(input_xx)
+print(sandip(input_xx))
