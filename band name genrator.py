1eab9cd7418075a7ba8bbd0e66d594779895111b
sandip-pathe <sandip.pathe@vit.edu.in>
2023-09-07 14:16:29 +0530

    Added new line \n.

--- band name genrator.py
+++ band name genrator.py
@@ -3,10 +3,10 @@
 print("Welcome to the band Generator")
 
 #2. Ask the user for the city that they grew up in.
-city = input("Which city did you grew up in? ")
+city = input("Which city did you grew up in?\n")
 
 #3. Ask the user for the name of a pet.
-pet_name = input("What is your pet name? ")
+pet_name = input("What is your pet name? \n")
 
 #4. Combine the name of their city and pet and show them their band name.
 print(f"your band name is {city} {pet_name} \n")
